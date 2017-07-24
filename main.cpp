eae80b885cc52b1295eb2c4479d20ada7df0fee9
hx19940102 <krishx@umich.edu>
2017-07-24 11:14:04 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+/********************************************************************
+* Project Name : Road track detection for agv                       *
+* Description  : Track the road for navigation                      *
+* Author       : Kris Huang                                         *
+* Date         : July 06, 2017                                      *
+* Notes        : This file is the main.cpp     				     	*
+*********************************************************************/
+
+
+#include "road_without_track.h"
+
+
+#define F_DX 806
+#define F_DY 806
+#define C_X 340
+#define C_Y 256
+
+
+int main() {
+	while (1) {
+		//Mat img = imread("road5.jpg");
+		int mode = 0;
+		cout << "Input 1 for road with track, 2 for road without track, 3 for using camera, 4 for quit : " << endl;
+		cin >> mode;
+		cout << endl;
+		if (mode == 1) {
+			string video_name = "";
+			cout << "Name of Video : " << endl;
+			cin >> video_name;
+			VideoCapture cam(video_name);
+			if (!cam.isOpened()) continue;
+			road_with_track* test = new road_with_track(500, F_DY, C_X, C_Y);
+			Mat frame;
+			while (1) {
+				cam >> frame;
+				if (frame.empty()) break;
+				double angle = test->agv_navigate(frame);
+				if (angle > 0) cout << "Trun Right : " << angle << " degree" << endl;
+				if (angle < 0) cout << "Trun Left : " << -angle << " degree" << endl;
+			}
+		}
+		if (mode == 2) {
+			string video_name = "";
+			cout << "Name of Video : " << endl;
+			cin >> video_name;
+			video_name = "ENOCHS_Videos//" + video_name;
+			VideoCapture cam(video_name);
+			if (!cam.isOpened()) continue;
+			road_without_track* test = new road_without_track(1103 / 4, F_DY, C_X, C_Y);
+			Mat frame;
+			while (1) {
+				cam >> frame;
+				if (frame.empty()) break;
+				double angle = test->agv_navigate(frame);
+				if (angle > 0) cout << "Trun Right : " << angle << " degree" << endl;
+				if (angle < 0) cout << "Trun Left : " << -angle << " degree" << endl;
+			}
+		}
+		if (mode == 3) {
+			VideoCapture cam(0);
+			if (!cam.isOpened()) continue;
+			road_without_track* test = new road_without_track(F_DX, F_DY, C_X, C_Y);
+			Mat frame;
+			while (1) {
+				cam >> frame;
+				if (frame.empty()) break;
+				double angle = test->agv_navigate(frame);
+				if (angle > 0) cout << "Trun Right : " << angle << " degree" << endl;
+				if (angle < 0) cout << "Trun Left : " << -angle << " degree" << endl;
+			}
+		}
+		if (mode == 4) break;
+		cout << endl;
+	}
+	return 0;
+}
---
+++ road_without_track.h
@@ -0,0 +1,45 @@
+/********************************************************************
+* Project Name : Road track detection for agv                       *
+* Description  : Track the road for navigation                      *
+* Author       : Kris Huang                                         *
+* Date         : July 06, 2017                                      *
+* Notes        : This file is road_without_track.h                  *
+*                for road without traffic lanes				     	*
+* CTI One Corporation, 3679 Enochs Street, Santa Clara, CA 95051    *
+*********************************************************************/
+
+
+#pragma once
+#ifndef ROAD_WITHOUT_TRACK_H
+#define ROAD_WITHOUT_TRACK_H
+
+
+#include "road_with_track.h"
+
+class findroad {
+private:
+	Mat path;
+public:
+	void setpath(Mat& image);
+	Mat getroad(Mat &image);
+};
+
+class road_without_track : public agv_navigation {
+private:
+	pair<double, double> track;
+public:
+	road_without_track(double a, double b, double c, double d) {
+		f_dx = a; f_dy = b; c_x = c; c_y = d;
+		track = pair<double, double>(0., 0.); }
+	Point agv_track_detection(Mat&);
+	double agv_contourSize(const vector<Point>&);
+	pair<vector<Point>, vector<Point>> agv_find_border_point(const Mat&, const Mat&);
+	pair<double, double> agv_linear_regression(const vector<Point>&);
+	vector<double> agv_quadratic_regression(const vector<Point>&);
+	void agv_reject_outliers(vector<Point>&);
+	double agv_navigate(const Mat&);
+	Mat agv_shadow_removal(const Mat &);
+};
+
+
+#endif
